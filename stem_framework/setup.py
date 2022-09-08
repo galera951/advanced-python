f32dc43065508ba760e7e64bf5ad706baa7564f9
Lugovtsov Gleb <galera951@gmail.com>
2022-09-08 23:24:07 +0300

    Fill setup.py

---
+++ stem_framework/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="stem",
+    version="1.0",
+    packages=["stem"],
+    install_requires=[
+        "Sphinx",
+        "Pylint",
+        "MyPy",
+    ],
+)
